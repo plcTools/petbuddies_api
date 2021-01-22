ca95b5d63efda3291c63891d206b0f64def998c7
JavierCaroelli <javiercaroelli@gmail.com>
2021-01-22 11:17:21 -0300

    Rutas petRoute

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import morgan from "morgan";
 import express from "express";
-import ownerRouter from './owner';
+import ownerRouter from "./owner";
+import petRoute from "./petRoute";
 
 const app = express.Router();
 app.use(express.json()); // Body Parser
 app.use(morgan("dev"));
 
 app.use ('/user', ownerRouter);
+app.use ('/pet', petRoute);
 
 export default app;
---
+++ src/routes/petRoute.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import Pet from "../models/Pet";
+const server = express.Router();
+
+server.post("/", async ( req, res ) => {
+  try {
+    const pet = await Pet.create(req.body);
+    await pet.save();
+    res.send(pet);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+server.get("/", async ( req, res ) => {
+  try {
+    const pets = await Pet.find();
+    res.send(pets);
+  } catch (error) {
+    res.send('No encontró')
+  }
+});
+
+server.get("/:id", async( req, res ) => {
+ try {
+   const pet = await Pet.findById(req.params.id)
+   res.send(pet);
+ } catch (error) {
+   res.send('no anduvo')
+ }
+})
+
+export default server;
