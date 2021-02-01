8bfdb9b0e31512f608bd5e5fa44fc17962085bd9
j6alvarez <javier96alvarez@gmail.com>
2021-02-01 15:28:37 -0300

    Hoteles Backend

---
+++ src/models/Hotels.ts
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+import moment from "moment";
+import "moment/locale/es";
+
+const HotelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    photo: {
+      type: String,
+      trim: true,
+    },
+    schedule: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    fee: {
+      type: Number,
+      required: true,
+    },
+    allowedPets: {
+      type: Array,
+      required: true,
+    },
+    foodInclude: {
+      type: Boolean,
+      default: false,
+    },
+    requirement: {
+      type: String,
+    },
+    zone: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    celphone: {
+      type: String,
+      required: true,
+    },
+    allowedNumber: {
+      // Cantidad de cupos disponibles
+      type: Number,
+    },
+    checkIn: {
+      type: Number,
+    },
+    checkOut: {
+      type: Number,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    extras:{
+      type:Array
+    }
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const Hotel = mongoose.model("Hotel", HotelSchema);
+
+export default Hotel;
