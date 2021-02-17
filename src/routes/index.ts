f0ef6e0673488773c0a8a9ac015d01ae3b417ba8
Manuel Bolla Agrelo <manubolla17@gmail.com>
2021-02-17 12:23:43 -0300

    fix reviews with redux and requests

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,7 @@ import reviewRouter from "./reviewRoute";
 
 const app = express.Router();
 app.use(express.json({limit: '25mb'}));
-app.use(express.urlencoded({limit: '25mb'})); // Body Parser
+app.use(express.urlencoded({ extended: true })); // Body Parser
 app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
